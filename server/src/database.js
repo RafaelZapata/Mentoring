2f90900423b464191c454b55c21da17357062a00
RafaelZapata <faelzapata2011@gmail.com>
2022-08-11 20:54:12 -0300

    Finalizado o CRUD

--- server/src/database.js
+++ server/src/database.js
@@ -2,13 +2,13 @@ const { MongoClient } = require("mongodb");
 
 const client = new MongoClient(process.env.DATABASE_URI);
 
-export async function connect() {
+module.exports = async function connect() {
   try {
     await client.connect();
 
     await client.db("admin").command({ ping: 1 });
 
-    return client;
+    return client.db("mentorship");
   } catch (e) {
     return e;
   }
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./database.js");
+const { ObjectId } = require("mongodb");
 
 const app = express();
 const port = 7002;
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
@@ -15,12 +22,74 @@ app.get("/", (req, res) => {
 app.get("/posts", async (req, res) => {
   const clientDB = await connectDB();
 
-  console.log(clientDB);
+  const collection = clientDB.collection("posts");
 
   res.json({
     status: true,
     code: 200,
-    posts: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    posts: await collection.find().toArray()
+  });
+});
+
+app.post("/posts", async (req, res) => {
+  const clientDB = await connectDB();
+
+  const collection = clientDB.collection("posts");
+
+  if(typeof req.body.tittle !== "string"){
+    return res.status(400).json({
+      status: false,
+      code: 400,
+      message: "O titulo precisa ser preenchido."
+    });
+  }
+
+  if (typeof req.body.author === "undefined"){
+    req.body.author = "Anonymous";
+  }
+
+  const data = await collection.insertOne(req.body);
+
+  res.json({
+    status: true,
+    code: 200,
+    data: data
+  });
+
+  res.redirect('/');
+});
+
+app.delete("/:id", async (req,res) =>{
+  
+  const clientDB = await connectDB();
+
+  const collection = clientDB.collection("posts"); 
+
+  await collection.deleteOne({ "_id": ObjectId(req.params.id) });
+
+  res.json({
+    status: true,
+    code: 200,
+    message: "Usuário deletado"
+  });
+});
+
+app.put("/update/:id", async (req, res) => {
+  const clientDB = await connectDB();
+
+  const collection = clientDB.collection("posts");
+
+  await collection.update({ "_id": ObjectId(req.params.id) }, {$set: {
+      tittle: req.body.tittle,
+      description: req.body.description,
+      author: req.body.author
+    }
+  });
+
+  res.json({
+    status: true,
+    code: 200,
+    message: "Usuário atualizado"
   });
 });
 
